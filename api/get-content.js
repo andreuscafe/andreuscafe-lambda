182dd0f9a72d8ce0ab418a1d87c0cb614cdebe5f
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2020-08-29 00:58:45 -0300

    Added messages from original chats

--- api/get-content.js
+++ api/get-content.js
@@ -1,11 +1,6 @@
 import content from "./content";
 
 module.exports = (req, res) => {
-    // res.json({
-    //   body: req.body,
-    //   query: req.query,
-    //   cookies: req.cookies,
-    // })
     const chat = req.query.key ? content[req.query.key] : "";
 
     res.json({
