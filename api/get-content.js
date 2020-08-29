0ac8a822f6b6adbb8aae0dff658fdd7ca86c67ee
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2020-08-29 00:34:15 -0300

    Trying to get this to work on vercel

---
+++ api/get-content.js
@@ -0,0 +1,11 @@
+module.exports = (req, res) => {
+    // res.json({
+    //   body: req.body,
+    //   query: req.query,
+    //   cookies: req.cookies,
+    // })
+
+    res.json({
+        body: "Prueba",
+    });
+};
