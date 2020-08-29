b8525db23c6ba669de68f10f42e15639d001501f
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2020-08-29 00:42:27 -0300

    Added a simple data file

---
+++ api/content.js
@@ -0,0 +1,5 @@
+module.exports = {
+    first: "Soy el primer post",
+    second: "Soy el segundo post",
+    "third post": "Yo soy el tercero",
+};
--- api/get-content.js
+++ api/get-content.js
@@ -1,12 +1,15 @@
+import content from "./content";
+
 module.exports = (req, res) => {
     // res.json({
     //   body: req.body,
     //   query: req.query,
     //   cookies: req.cookies,
     // })
+    const chat = req.query.key ? content[req.query.key] : "";
 
     res.json({
-        body: "Prueba",
-        query: req.query,
+        key: req.query.key,
+        body: chat,
     });
 };
